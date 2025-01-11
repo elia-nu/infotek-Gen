a36203b8b10241f3c90737a8aa327bbafc577b05
elia-nu <elias.nuruad@gmail.com>
2025-01-11 17:22:59 +0300

    Enhance middleware and server configuration by adding CORS support and refactoring data fetching in Home component. The middleware now sets CORS headers and handles preflight requests, while the server uses Express for improved routing and CORS management. Consolidated API calls in Home component for better readability and efficiency.

--- middleware.js
+++ middleware.js
@@ -1,10 +1,21 @@
 // middleware.js
 import { NextResponse } from 'next/server';
 
-export function middleware(request) {
-    return NextResponse.next();
-}
+export function middleware(req) {
+  const res = NextResponse.next();
 
-export const config = {
-    matcher: '/api/:path*',
-};
+  // Set CORS headers
+  res.headers.set('Access-Control-Allow-Origin', '*'); // Change '*' to your specific origin if needed
+  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: res.headers,
+    });
+  }
+
+  return res;
+}
--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+    
+
     images: {
       unoptimized: true,  // If you are using Next.js Image Optimization
-    },
-    env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    },
-    async headers() {
-      return [
-        {
-          source: '/:path*',
-          headers: [
-            {
-              key: 'Access-Control-Allow-Origin',
-              value: '*',
-            },
-          ],
-        },
-      ];
-    },
-}
+    },}
 
 module.exports = nextConfig
