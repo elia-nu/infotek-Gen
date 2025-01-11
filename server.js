eea0fc5eb9769243fc37ef3861d903c425a884da
elia-nu <elias.nuruad@gmail.com>
2025-01-11 15:48:44 +0300

    yt8\

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const next = require("next");
+const cors = require('cors');
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -7,6 +8,14 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+
+  // Add CORS middleware
+  server.use(cors({
+    origin: process.env.NODE_ENV === 'production' 
+      ? ['https://www.genshifter.com']
+      : ['http://localhost:3000'],
+    credentials: true
+  }));
 
   // Define custom routes here, if needed
   server.get("/custom-route", (req, res) => {
